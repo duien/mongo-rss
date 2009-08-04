dd38183ae609f098d58273a39d6ffe6bcf217609
John Hyland <github@djspinmonkey.com>
2009-08-04 16:07:07 -0400

    fixed hotness order

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -53,7 +53,7 @@ describe User do
 
       it "should order the items by hotness" do
         pending "implement hotness signatures" do
-          @user.unread_items( :order => :hottest ).should eql [ @items[4], @items[0], @items[2], @items[1], @items[3] ]
+          @user.unread_items( :order => :hottest ).should eql [ @items[4], @items[0], @items[2], @items[3], @items[1] ]
         end
       end
 
