6ea7ebf1f62fd39247f1187d8ac1408315f7caa5
John Hyland <github@djspinmonkey.com>
2009-08-04 14:15:09 -0400

    flesh out items a bit

--- lib/item.rb
+++ lib/item.rb
@@ -4,5 +4,9 @@ class Item
   include MongoMapper::Document
 
   # database 'mongo-rss'
+  key :guid, String  # This refers to the id provided by the feed, not the database id
+  key :link, String
+  key :title, String
   key :body, String
+  key :published_at, Time
 end
