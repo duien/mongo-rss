a28a5f7f156580fa0acb66846323243c7767e1e1
John Hyland <github@djspinmonkey.com>
2009-08-04 14:16:15 -0400

    starting on sorting unread items

--- lib/user.rb
+++ lib/user.rb
@@ -11,12 +11,20 @@ class User
   # STUB - fix the tests once removed
   attr_accessor :items
 
+  SORTING_STRATEGIES = {
+#   :hottest => lambda { |a, b| a.},
+#   :newest => lambda {},
+#   :oldest => lambda {},
+    :unordered => lambda { |a, b| 0 }
+  }
+
   # All the unread items from all of this user's feeds.  The options hash can
   # take the following keys:
   #
   #     :order => one of :newest, :oldest, :hottest, or :unordered
   #
   def unread_items (options = {})
+    order = options[:order] || :hottest
     @items    # TODO: implement this
   end
 end
