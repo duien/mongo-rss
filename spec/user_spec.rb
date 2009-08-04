3c5799f080701ffa9d9c90e6f6a408b9f8980ed3
John Hyland <github@djspinmonkey.com>
2009-08-04 14:17:51 -0400

    more testiness

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -5,12 +5,13 @@ require File.join( File.dirname(__FILE__), '../lib/item.rb' )
 
 describe User do
 
-  before(:all) do
+  before do
     @user = User.new
   end
 
   context "with unread items" do
-    before(:each) do
+    before do
+      # TODO: once User#items is removed, refactor this
       @user.items = [ Item.new(:body => "Card games are good and interesting and I love them."),
                       Item.new(:body => "Politics are boring and I don't want to read about them."),
                       Item.new(:body => "Books are also interesting and I love them too"),
@@ -21,6 +22,10 @@ describe User do
 
     it "should have some unread items" do
       @user.unread_items.should_not be nil
+    end
+
+    it "should order the items by hotness" do
+      pending "implement bayesian hotness"
     end
 
   end
