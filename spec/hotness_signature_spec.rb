aece61cd9e9e96dcb7086d51e5ab752f27f2e1ba
John Hyland <github@djspinmonkey.com>
2009-08-11 15:24:16 -0400

    Testiness.

--- spec/hotness_signature_spec.rb
+++ spec/hotness_signature_spec.rb
@@ -1,13 +1,33 @@
 # hotness_signature_spec.rb
 
 require File.join( File.dirname(__FILE__), '../lib/hotness_signature.rb' )
+require File.join( File.dirname(__FILE__), '../lib/user.rb' )
 
 describe HotnessSignature do
 
+  before do
+    @user = User.new
+    @sig = HotnessSignature.new
+    @user.hotness_signature = @sig
+  end
+
   context "when newly created" do
 
-    before do
-      @sig = HotnessSignature.new
+    it "should be trainable" do
+      @sig.train( "This is some text.", 1)
+    end
+
+  end
+
+  context "after being trained" do
+    
+    before do 
+      @sig.train( "good awesome wonderful interesting words", 300 )
+      @sig.train( "boring crappy skipped sucky words", 3 )
+    end
+
+    it "should predict text with good words higher than text with bad words" do
+      ( @sig.predict("boring crappy stuff") < @sig.predict("awesome interesting things") ).should be_true
     end
 
   end
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -2,6 +2,7 @@
 
 require File.join( File.dirname(__FILE__), '../lib/user.rb' )
 require File.join( File.dirname(__FILE__), '../lib/item.rb' )
+require File.join( File.dirname(__FILE__), '../lib/hotness_signature.rb' )
 
 describe User do
 
@@ -10,20 +11,21 @@ describe User do
   end
 
   it "should be able to read an item" do
-    @user.read( Item.new(), 10 )
+    @user.read( Item.new( :body => "Some text!" ), 10 )
   end
 
   context "with unread items" do
     before do
-      @items = [ Item.new(:published_at => Time.at(100), :body => "Card games are good and interesting and I love them."),
-                 Item.new(:published_at => Time.at(200), :body => "Politics are boring and I don't want to read about them."),
-                 Item.new(:published_at => Time.at(300), :body => "Books are also interesting and I love them too"),
+      @items = [ Item.new(:published_at => Time.at(100), :body => "Card games are interesting."),
+                 Item.new(:published_at => Time.at(200), :body => "Politics are boring."),
+                 Item.new(:published_at => Time.at(300), :body => "Books are also interesting."),
                  Item.new(:published_at => Time.at(400), :body => "Politics are still boring and they suck."),
-                 Item.new(:published_at => Time.at(500), :body => "Books and games are still interesting and I super love them.")
+                 Item.new(:published_at => Time.at(500), :body => "Books and games are still interesting and I love them.")
                ]
 
       # TODO: once User#items is removed, refactor this
       @user.items = @items
+      @user.hotness_signature = HotnessSignature.new
     end
 
     it "should have some unread items" do
@@ -44,17 +46,15 @@ describe User do
 
     context "with a hotness signature" do
       before do
-        crappy_item = Item.new( :body => "Politics boring blah blah blah lots of words this sucks" )
-        groovy_item = Item.new( :body => "Books and card games are interesting I super love them!" )
+        crappy_item = Item.new( :body => "Politics boring sucks" )
+        groovy_item = Item.new( :body => "Books books card games games interesting love" )
 
         @user.read crappy_item, 3     # We blew past this crappy article in 3 seconds
         @user.read groovy_item, 300   # We spent five minutes reading this groovy one
       end
 
       it "should order the items by hotness" do
-        pending "implement hotness signatures" do
-          @user.unread_items( :order => :hottest ).should eql [ @items[4], @items[0], @items[2], @items[3], @items[1] ]
-        end
+        @user.unread_items( :order => :hottest ).should eql [ @items[4], @items[0], @items[2], @items[1], @items[3] ]
       end
 
     end
