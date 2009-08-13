2fa6d91f231062ae3265858ebb6d9b6b8ed3e53f
Emily Price <price.emily@gmail.com>
2009-08-12 20:26:04 -0400

    Added more attributes to item and constructor from feedzirra entry

--- lib/item.rb
+++ lib/item.rb
@@ -7,6 +7,31 @@ class Item
   key :guid, String  # This refers to the id provided by the feed, not the database id
   key :link, String
   key :title, String
-  key :body, String
+  key :author, String
+  key :content, String
+  key :summary, String
+  key :categories, Array
+  key :links, Array
+  key :updated_at, Time
   key :published_at, Time
+  
+  def self.new_from_feedzirra( entry )
+    new(
+      :guid => entry.id,
+      :link => entry.url,
+      :title => entry.title,
+      :author => entry.author,
+      :content => entry.content,
+      :summary => entry.summary,
+      :categories => entry.categories,
+      :links => entry.links,
+      :updated_at => entry.updated,
+      :published_at => entry.published
+    )
+  end
+
+  def body
+    # content or summary as appropriate
+    content || summary
+  end
 end
