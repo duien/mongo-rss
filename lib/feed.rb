848c9b5b21a24720495e85581f873ed05d478005
Emily Price <price.emily@gmail.com>
2009-08-12 20:25:33 -0400

    Feeds can update. Using feedzirra instead of feed normalizer

--- lib/feed.rb
+++ lib/feed.rb
@@ -1,57 +1,80 @@
 require 'mongomapper'
-require 'feed-normalizer'
+require 'feedzirra'
 require 'open-uri'
 
 class Feed
   include MongoMapper::Document
 
   # database 'mongo-rss'
-  key :title, String
-  key :description, String
-  key :feed_id, String
-  key :last_updated, Time
-  key :copyright, String
-  key :authors, Array
-  key :site_urls, Array
-  key :generator, String
-  key :ttl, String
-  key :skip_hours, String
-  key :skip_days, String
 
-  key :url, String
+  # The feed's information about itself
+  key :title,                  String
+  key :feed_id,                String
+  key :last_modified,          Time
+  key :link,                   String # A link the the site
+  key :etag,                   String
+  
+  # My information about the feed
+  key :url,                    String # The URL of the feed
   key :last_successful_update, Time
-  key :last_attempted_update, Time
+  key :last_attempted_update,  Time
+
+  # For Feedzirraing
+  key :type,                   String
+
+  many :items
 
   def update!
-    update_time = Time.now
-    last_attempted_update = update_time
-    begin
-      feed = FeedNormalizer::FeedNormalizer.parse open( url )
-      feed.clean!
-
-      # do we actually need to do anything?
-      return if feed.last_updated and last_successful_update and feed.last_updated < last_successful_update
-
-      # update any attributes on the feed
-      FeedNormalizer::Feed::ELEMENTS.each do |element|
-        key = case element
-              when :urls
-                :site_urls
-              when :id
-                :feed_id
-              when :items, :image
-                next
-              else
-                element
-              end
-        self[key] = feed.send( element ) unless feed.send( element ).nil?
+    with_updates = Feedzirra::Feed.update( feedzirra )
+    if with_updates.updated?
+      # update attributes
+      title = with_updates.title
+      link  = with_updates.url
+    end
+    if with_updates.has_new_entries?
+      # do stuff with the new entries
+      with_updates.new_entries.each do |entry|
+        entry.sanitize!
+        new_item = Item.new_from_feedzirra( entry )
+        items << new_item
       end
-      last_successful_update = update_time
-    rescue
-      raise
-    ensure
-      save
+    end
+
+    # always update etag and modified
+    etag          = with_updates.etag
+    last_modified = with_updates.last_modified
+    @feedzirra = with_updates
+    save
+  end
+
+  def feedzirra
+    if @feedzirra.nil?
+      if type
+        @feedzirra = eval(type).new
+        @feedzirra.feed_url = url
+        @feedzirra.last_modified = last_modified
+        @feedzirra.etag = etag
+        @feedzirra.entries = items.last
+      else
+        @feedzirra = Feedzirra::Feed.fetch_and_parse( url )
+        do_first_update
+      end
+    end
+    @feedzirra
+  end
+
+  def do_first_update
+    self.title = @feedzirra.title
+    self.link = @feedzirra.url
+    self.type = @feedzirra.class
+    self.etag = @feedzirra.etag
+    self.last_modified = @feedzirra.last_modified
+    @feedzirra.entries.each do |entry|
+      entry.sanitize!
+      new_item = Item.new_from_feedzirra( entry )
+      items << new_item
     end
   end
+
 end
 
